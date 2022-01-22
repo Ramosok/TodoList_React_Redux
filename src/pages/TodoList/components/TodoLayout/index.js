440d04be4a9e118107186adb74e503a7a272a19c
Ramosok <Ramosok86@gmail.com>
2022-01-22 05:20:01 +0300

    first commit

---
+++ src/pages/TodoList/components/TodoLayout/index.js
@@ -0,0 +1,9 @@
+
+export const TodoLayout = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
---
+++ src/store/configureStore.js
@@ -0,0 +1,8 @@
+import {createStore} from "redux";
+import {composeWithDevTools} from "redux-devtools-extension";
+
+import rootReduser from './rootReduser'
+
+export const createStore = () => {
+    const store = createStore
+}
